553468ca5f02039d7a3e4f57c29f689203400d12
Dimororo <32198480+Dimororo@users.noreply.github.com>
2018-06-06 15:31:25 +0300

    yoppe

--- src/main/resources/static/js/classBook.js
+++ src/main/resources/static/js/classBook.js
@@ -1,30 +1,7 @@
 function createNewStudent() {
-    $('.Students').append('<td contenteditable='true'> NameOfStudent</td>
-    <td contenteditable="true" id="date1">1</td>
-    <td contenteditable="true" id="date2">2</td>
-    <td contenteditable="true" id="date3">3</td>
-    <td contenteditable="true" id="date4">4</td>
-    <td contenteditable="true" id="date5">5</td>
-    <td contenteditable="true" id="date6">6</td>
-    <td contenteditable="true" id="date7">7</td>
-    <td contenteditable="true" id="date8">8</td>
-    <td contenteditable="true" id="date9">9</td>
-    <td contenteditable="true" id="date10">10</td>
-    <td contenteditable="true" id="date11">11</td>
-    <td contenteditable="true" id="date12">12</td>
-    <td contenteditable="true" id="date13">13</td>
-    <td contenteditable="true" id="date14">14</td>
-    <td contenteditable="true" id="date15">15</td>
-    <td contenteditable="true" id="date16">16</td>
-    <td contenteditable="true" id="date17">17</td>
-    <td contenteditable="true" id="date18">18</td>
-    <td contenteditable="true" id="date19">19</td>
-    <td contenteditable="true" id="date20">20</td>
-    <td contenteditable="true" id="date21">21</td>
-    <td contenteditable="true" id="date22">22</td>
-    <td contenteditable="true" id="date23">23</td>
-    <td contenteditable="true" id="date24">24</td>
-    <td contenteditable="true" id="date25">25</td>'
-)
-    ;
+    for (i = 0; i<4; i++){
+        for (j = 0; j<25; j++){
+            document.getElementById.("student" + i + "_" + j).value = value(i);
+        }
+    }
 }
